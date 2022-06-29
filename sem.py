dff682ed5fe61ada948eab678c269028657aca82
vbear97 <102497049+vbear97@users.noreply.github.com>
2022-06-29 18:30:30 +1000

    if not using inv-gamma from pyro, don't import pyro

--- sem.py
+++ sem.py
@@ -5,7 +5,7 @@ from sklearn.cluster import k_means
 import torch
 from torch.distributions import Normal, Gamma, Binomial
 from torch.distributions import MultivariateNormal as mvn
-import pyro
+#import pyro
 #from pyro.distributions import InverseGamma
 from torch.utils.tensorboard import SummaryWriter
 
