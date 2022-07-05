de0e5fc8cc2825b99b6864618342d610d9d679b2
vbear97 <102497049+vbear97@users.noreply.github.com>
2022-07-05 15:59:16 +1000

    comment out unncessary, add in mcmc hyperparmms and arviz

--- MCMC.py
+++ MCMC.py
@@ -7,10 +7,11 @@ from torch.distributions import MultivariateNormal as mvn
 #import loop packages 
 from tqdm import trange
 
-#import pystan packages 
+#import relevant mcmcpystan packages 
 import nest_asyncio
 nest_asyncio.apply()
 import stan
+import arviz as az
 
 #import relevant packages
 import numpy
@@ -19,49 +20,50 @@ import numpy as np
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
+
 # %%
-sig2_shape = torch.tensor([1.0])  
-sig2_rate = torch.tensor([1.0])  
+# sig2_shape = torch.tensor([1.0])  
+# sig2_rate = torch.tensor([1.0])  
 
-#psi ~ iid Inv Gamma for j = 1..m 
-psi_shape = torch.tensor([1.0])  
-psi_rate = torch.tensor([1.0])  
+# #psi ~ iid Inv Gamma for j = 1..m 
+# psi_shape = torch.tensor([1.0])  
+# psi_rate = torch.tensor([1.0])  
 
-#nu ~ iid Normal for j = 1...m
-nu_sig2 = torch.tensor([10.0])  
-nu_mean = torch.tensor([0.0])
+# #nu ~ iid Normal for j = 1...m
+# nu_sig2 = torch.tensor([10.0])  
+# nu_mean = torch.tensor([0.0])
 
-#lam_j | psi_j ~ id Normal(mu, sig2*psi_j)
-lam_mean = torch.tensor([1.0])
-lam_sig2 = torch.tensor([10.0])
+# #lam_j | psi_j ~ id Normal(mu, sig2*psi_j)
+# lam_mean = torch.tensor([1.0])
+# lam_sig2 = torch.tensor([10.0])
 
-hyper = {"sig2_shape": sig2_shape, "sig2_rate": sig2_rate, "psi_shape": psi_shape, "psi_rate": psi_rate, "nu_sig2": nu_sig2, "nu_mean": nu_mean, "lam_mean": lam_mean, "lam_sig2": lam_sig2}
+# hyper = {"sig2_shape": sig2_shape, "sig2_rate": sig2_rate, "psi_shape": psi_shape, "psi_rate": psi_rate, "nu_sig2": nu_sig2, "nu_mean": nu_mean, "lam_mean": lam_mean, "lam_sig2": lam_sig2}
 
 # %% 
 #Set True Values for Parameters 
-N = 1000
-M = 3
-nu = torch.tensor([5.0, 10.0, 2.0])
-sig = torch.tensor([1.2])
-sig2= torch.square(sig)
-lam = torch.tensor([0.8, 0.5])
-psi_sqrt = torch.tensor([3.1, 2.2, 1.1])
-psi = torch.square(psi_sqrt)
+# N = 1000
+# M = 3
+# nu = torch.tensor([5.0, 10.0, 2.0])
+# sig = torch.tensor([1.2])
+# sig2= torch.square(sig)
+# lam = torch.tensor([0.8, 0.5])
+# psi_sqrt = torch.tensor([3.1, 2.2, 1.1])
+# psi = torch.square(psi_sqrt)
 
-#Generate Latent Variables
-eta = torch.randn(N)*sig
-lam1 = torch.tensor([1.0])
-lam_full= torch.cat((lam1, lam))
+# #Generate Latent Variables
+# eta = torch.randn(N)*sig
+# lam1 = torch.tensor([1.0])
+# lam_full= torch.cat((lam1, lam))
 
 # %%
 #Generate y values based on User Input
 # yi ~ id Normal(nu + eta_i * lam, diag(psi)), yi /in R^m
 #cov:
-like_dist_cov = torch.diag(psi) #m*m tensor 
-#means: want a n*m vector of means
-like_dist_means = torch.matmul(eta.unsqueeze(1), lam_full.unsqueeze(0)) + nu
-#Generate yi
-y_data = mvn(like_dist_means, covariance_matrix= like_dist_cov).rsample() #n*m tensor
+# like_dist_cov = torch.diag(psi) #m*m tensor 
+# #means: want a n*m vector of means
+# like_dist_means = torch.matmul(eta.unsqueeze(1), lam_full.unsqueeze(0)) + nu
+# #Generate yi
+# y_data = mvn(like_dist_means, covariance_matrix= like_dist_cov).rsample() #n*m tensor
 
 # %%
 #Write mcmc function 
@@ -142,9 +144,11 @@ h = {var:param.item() for var,param in hyper.items()}
 data.update(h)
 
 #sample and visualise 
-
-#don't need: 
-#get rid of nu_mean, hardset to 0
+posterior = mc(data)
+fit = posterior.sample(num_chains = 4, num_warmup = 7500, num_samples = 15000)
 
 # %%
-#rough diagnostics: check the arithmetic mean, mode and variance 
+#MCMC Diagnostics
+
+# %%
+#MCMC Visualisation 
