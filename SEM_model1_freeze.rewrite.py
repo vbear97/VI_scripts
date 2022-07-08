f6812b5699ff1d4fb3c76e512d9a22da9066085e
vbear97 <102497049+vbear97@users.noreply.github.com>
2022-07-08 13:05:56 +1000

    changed num-warmup and num_samples to variables rather than type in

--- SEM_model1_freeze.rewrite.py
+++ SEM_model1_freeze.rewrite.py
@@ -193,7 +193,7 @@ data.update(h)
 # %%
 #Main Part 2: Do MCMC
 posterior = mc(data)
-fit = posterior.sample(num_chains = 4, num_warmup = 1000, num_samples = 10000)
+fit = posterior.sample(num_chains = 4, num_warmup = num_warmup, num_samples = num_samples)
 fitp = fit.to_frame() #convert to pandas data frame
 var = ['nu.1', 'nu.2', 'nu.3', 'lam.1', 'lam.2', 'psi.1', 'psi.2', 'psi.3', 'sig2']  #hard coded order, not efficient: nu, lam, psi,sig2
 
