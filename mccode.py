9766c2d425d9cc7dae4bcc1f92c26bebd88bed7f
vbear97 <102497049+vbear97@users.noreply.github.com>
2022-07-12 14:25:03 +1000

    comment out everything but function in mccode

--- mccode.py
+++ mccode.py
@@ -22,48 +22,48 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 # %%
-sig2_shape = torch.tensor([1.0])  
-sig2_rate = torch.tensor([1.0])  
+# sig2_shape = torch.tensor([1.0])  
+# sig2_rate = torch.tensor([1.0])  
 
-#psi ~ iid Inv Gamma for j = 1..m 
-psi_shape = torch.tensor([1.0])  
-psi_rate = torch.tensor([1.0])  
+# #psi ~ iid Inv Gamma for j = 1..m 
+# psi_shape = torch.tensor([1.0])  
+# psi_rate = torch.tensor([1.0])  
 
-#nu ~ iid Normal for j = 1...m
-nu_sig2 = torch.tensor([10.0])  
-nu_mean = torch.tensor([0.0])
+# #nu ~ iid Normal for j = 1...m
+# nu_sig2 = torch.tensor([10.0])  
+# nu_mean = torch.tensor([0.0])
 
-#lam_j | psi_j ~ id Normal(mu, sig2*psi_j)
-lam_mean = torch.tensor([1.0])
-lam_sig2 = torch.tensor([10.0])
+# #lam_j | psi_j ~ id Normal(mu, sig2*psi_j)
+# lam_mean = torch.tensor([1.0])
+# lam_sig2 = torch.tensor([10.0])
 
-hyper = {"sig2_shape": sig2_shape, "sig2_rate": sig2_rate, "psi_shape": psi_shape, "psi_rate": psi_rate, "nu_sig2": nu_sig2, "nu_mean": nu_mean, "lam_mean": lam_mean, "lam_sig2": lam_sig2}
+# hyper = {"sig2_shape": sig2_shape, "sig2_rate": sig2_rate, "psi_shape": psi_shape, "psi_rate": psi_rate, "nu_sig2": nu_sig2, "nu_mean": nu_mean, "lam_mean": lam_mean, "lam_sig2": lam_sig2}
 
 # %% 
 #Set True Values for Parameters 
-N = 1000
-M = 3
-nu = torch.tensor([5.0, 10.0, 2.0])
-sig = torch.tensor([1.2])
-sig2= torch.square(sig)
-lam = torch.tensor([0.8, 0.5])
-psi_sqrt = torch.tensor([3.1, 2.2, 1.1])
-psi = torch.square(psi_sqrt)
+# N = 1000
+# M = 3
+# nu = torch.tensor([5.0, 10.0, 2.0])
+# sig = torch.tensor([1.2])
+# sig2= torch.square(sig)
+# lam = torch.tensor([0.8, 0.5])
+# psi_sqrt = torch.tensor([3.1, 2.2, 1.1])
+# psi = torch.square(psi_sqrt)
 
-#Generate Latent Variables
-eta = torch.randn(N)*sig
-lam1 = torch.tensor([1.0])
-lam_full= torch.cat((lam1, lam))
+# #Generate Latent Variables
+# eta = torch.randn(N)*sig
+# lam1 = torch.tensor([1.0])
+# lam_full= torch.cat((lam1, lam))
 
-# %%
-#Generate y values based on User Input
-# yi ~ id Normal(nu + eta_i * lam, diag(psi)), yi /in R^m
-#cov:diag(psi)
-like_dist_cov = torch.diag(psi) #m*m tensor 
-#means: want a n*m vector of means
-like_dist_means = torch.matmul(eta.unsqueeze(1), lam_full.unsqueeze(0)) + nu
-#Generate yi
-y_data = mvn(like_dist_means, covariance_matrix= like_dist_cov).rsample() #n*m tensor
+# # %%
+# #Generate y values based on User Input
+# # yi ~ id Normal(nu + eta_i * lam, diag(psi)), yi /in R^m
+# #cov:diag(psi)
+# like_dist_cov = torch.diag(psi) #m*m tensor 
+# #means: want a n*m vector of means
+# like_dist_means = torch.matmul(eta.unsqueeze(1), lam_full.unsqueeze(0)) + nu
+# #Generate yi
+# y_data = mvn(like_dist_means, covariance_matrix= like_dist_cov).rsample() #n*m tensor
 
 # %%
 #Write mcmc function 
